edc66a28f7bdbe972bece2df2c07468f3a0482af
abhishektiwari003 <abhishektiwari@gofynd.com>
2026-04-13 09:09:11 +0530

    unistall

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const PORT = 8080;
 const server = http.createServer((req, res) => {
   if (req.method === "GET" && req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Monday 13.04.2026 april testing 2026");
+    res.end(
+      "Monday updated the permission or install it again april testing 2026",
+    );
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Not Found");
